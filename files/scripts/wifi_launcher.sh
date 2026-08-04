c8a680446e8fe916250689295fe90174d49921de
Nathaniel Russell <46272571+nater1983@users.noreply.github.com>
2026-08-03 20:49:59 -0500

    Update wifi_launcher.sh

--- files/scripts/wifi_launcher.sh
+++ files/scripts/wifi_launcher.sh
@@ -1,38 +1,49 @@
 #!/usr/bin/env bash
-
 notify-send "Getting list of available Wi-Fi networks..."
-# Get a list of available wifi connections and morph it into a nice-looking list
-wifi_list=$(nmcli --fields "SECURITY,SSID" device wifi list | sed 1d | sed 's/  */ /g' | sed -E "s/WPA*.?\S/ /g" | sed "s/^--/ /g" | sed "s/  //g" | sed "/--/d")
 
 connected=$(nmcli -fields WIFI g)
 if [[ "$connected" =~ "enabled" ]]; then
-	toggle="睊  Disable Wi-Fi"
+	toggle="睊  Disable Wi-Fi"
 elif [[ "$connected" =~ "disabled" ]]; then
-	toggle="直  Enable Wi-Fi"
+	toggle="直  Enable Wi-Fi"
 fi
 
-# Use rofi to select wifi network
-chosen_network=$(echo -e "$toggle\n$wifi_list" | uniq -u | rofi -dmenu -i -selected-row 1 -p "Wi-Fi SSID: " )
-# Get name of connection
+# Terse, colon-separated output -- stable regardless of field width/content.
+# Escaped colons in SSIDs (nmcli escapes ':' as '\:') are left alone here.
+wifi_list=$(nmcli -t --fields SECURITY,SSID device wifi list | \
+	awk -F: '
+		{
+			ssid = $2
+			for (i = 3; i <= NF; i++) ssid = ssid ":" $i  # rejoin if SSID had a colon
+			if (ssid == "") next
+			sec = $1
+			icon = (sec == "" || sec == "--") ? "  " : "  "
+			print icon ssid
+		}
+	' | awk '!seen[$0]++')   # dedupe by full display line, order-preserving
+
+chosen_network=$(echo -e "$toggle\n$wifi_list" | rofi -dmenu -i -selected-row 1 -p "Wi-Fi SSID: ")
 chosen_id=$(echo "${chosen_network:3}" | xargs)
 
 if [ "$chosen_network" = "" ]; then
 	exit
-elif [ "$chosen_network" = "直  Enable Wi-Fi" ]; then
+elif [ "$chosen_network" = "直  Enable Wi-Fi" ]; then
 	nmcli radio wifi on
-elif [ "$chosen_network" = "睊  Disable Wi-Fi" ]; then
+elif [ "$chosen_network" = "睊  Disable Wi-Fi" ]; then
 	nmcli radio wifi off
 else
-	# Message to show when connection is activated successfully
 	success_message="You are now connected to the Wi-Fi network \"$chosen_id\"."
-	# Get saved connections
 	saved_connections=$(nmcli -g NAME connection)
-	if [[ $(echo "$saved_connections" | grep -w "$chosen_id") = "$chosen_id" ]]; then
+
+	if [[ $(echo "$saved_connections" | grep -Fxw "$chosen_id") = "$chosen_id" ]]; then
 		nmcli connection up id "$chosen_id" | grep "successfully" && notify-send "Connection Established" "$success_message"
 	else
-		if [[ "$chosen_network" =~ "" ]]; then
-			wifi_password=$(rofi -dmenu -p "Password: " )
+		security=$(nmcli -t --fields SECURITY,SSID device wifi list | awk -F: -v s="$chosen_id" '$2==s{print $1; exit}')
+		if [[ -n "$security" && "$security" != "--" ]]; then
+			wifi_password=$(rofi -dmenu -p "Password: ")
+			nmcli device wifi connect "$chosen_id" password "$wifi_password" | grep "successfully" && notify-send "Connection Established" "$success_message"
+		else
+			nmcli device wifi connect "$chosen_id" | grep "successfully" && notify-send "Connection Established" "$success_message"
 		fi
-		nmcli device wifi connect "$chosen_id" password "$wifi_password" | grep "successfully" && notify-send "Connection Established" "$success_message"
 	fi
 fi

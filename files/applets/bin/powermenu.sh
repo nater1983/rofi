1e525d08e081e896cc26b6be15d0d4a555761be3
Nathaniel Russell <46272571+nater1983@users.noreply.github.com>
2026-08-03 21:58:01 -0500

    Refactor power menu script for clarity and structure

--- files/applets/bin/powermenu.sh
+++ files/applets/bin/powermenu.sh
@@ -1,75 +1,50 @@
 #!/usr/bin/env bash
 
-## Author  : Aditya Shakya (adi1090x)
-## Github  : @adi1090x
+## Author : Aditya Shakya (adi1090x)
+## Github : @adi1090x
 #
-## Applets : Power Menu
+## Rofi   : Power Menu
+#
+## Available Styles
+#
+## style-1   style-2   style-3   style-4   style-5
 
-# Import Current Theme
-source "$HOME"/.config/rofi/applets/shared/theme.bash
-theme="$type/$style"
+# Current Theme
+dir="$HOME/.config/rofi/powermenu/type-1"
+theme='style-4'
 
-# Theme Elements
-prompt="`hostname`"
-mesg="Uptime : `uptime -p | sed -e 's/up //g'`"
-
-if [[ ( "$theme" == *'type-1'* ) || ( "$theme" == *'type-3'* ) || ( "$theme" == *'type-5'* ) ]]; then
-	list_col='1'
-	list_row='6'
-elif [[ ( "$theme" == *'type-2'* ) || ( "$theme" == *'type-4'* ) ]]; then
-	list_col='6'
-	list_row='1'
-fi
+# CMDs
+uptime="`uptime -p | sed -e 's/up //g'`"
+host=`hostname`
 
 # Options
-layout=`cat ${theme} | grep 'USE_ICON' | cut -d'=' -f2`
-if [[ "$layout" == 'NO' ]]; then
-	option_1=" Lock"
-	option_2=" Logout"
-	option_3=" Suspend"
-	option_4=" Hibernate"
-	option_5=" Reboot"
-	option_6=" Shutdown"
-	yes=' Yes'
-	no=' No'
-else
-	option_1=""
-	option_2=""
-	option_3=""
-	option_4=""
-	option_5=""
-	option_6=""
-	yes=''
-	no=''
-fi
+shutdown=' Shutdown'
+reboot=' Reboot'
+lock=' Lock'
+suspend=' Suspend'
+logout=' Logout'
+yes=' Yes'
+no=' No'
 
 # Rofi CMD
 rofi_cmd() {
-	rofi -theme-str "listview {columns: $list_col; lines: $list_row;}" \
-		-theme-str 'textbox-prompt-colon {str: "";}' \
-		-dmenu \
-		-p "$prompt" \
-		-mesg "$mesg" \
-		-markup-rows \
-		-theme ${theme}
-}
-
-# Pass variables to rofi dmenu
-run_rofi() {
-	echo -e "$option_1\n$option_2\n$option_3\n$option_4\n$option_5\n$option_6" | rofi_cmd
+	rofi -dmenu \
+		-p "Goodbye ${USER}" \
+		-mesg "Uptime: $uptime" \
+		-theme ${dir}/${theme}.rasi
 }
 
 # Confirmation CMD
 confirm_cmd() {
-	rofi -theme-str 'window {location: center; anchor: center; fullscreen: false; width: 350px;}' \
-		-theme-str 'mainbox {orientation: vertical; children: [ "message", "listview" ];}' \
+	rofi -theme-str 'window {location: center; anchor: center; fullscreen: false; width: 250px;}' \
+		-theme-str 'mainbox {children: [ "message", "listview" ];}' \
 		-theme-str 'listview {columns: 2; lines: 1;}' \
 		-theme-str 'element-text {horizontal-align: 0.5;}' \
 		-theme-str 'textbox {horizontal-align: 0.5;}' \
 		-dmenu \
 		-p 'Confirmation' \
 		-mesg 'Are you Sure?' \
-		-theme ${theme}
+		-theme ${dir}/${theme}.rasi
 }
 
 # Ask for confirmation
@@ -77,53 +52,69 @@ confirm_exit() {
 	echo -e "$yes\n$no" | confirm_cmd
 }
 
-# Confirm and execute
-confirm_run () {	
-	selected="$(confirm_exit)"
-	if [[ "$selected" == "$yes" ]]; then
-        ${1} && ${2} && ${3}
-    else
-        exit
-    fi	
+# Pass variables to rofi dmenu
+run_rofi() {
+	echo -e "$lock\n$suspend\n$logout\n$reboot\n$shutdown" | rofi_cmd
 }
 
 # Execute Command
 run_cmd() {
-	if [[ "$1" == '--opt1' ]]; then
-		betterlockscreen -l
-	elif [[ "$1" == '--opt2' ]]; then
-		confirm_run 'kill -9 -1'
-	elif [[ "$1" == '--opt3' ]]; then
-		confirm_run 'mpc -q pause' 'amixer set Master mute' 'systemctl suspend'
-	elif [[ "$1" == '--opt4' ]]; then
-		confirm_run 'systemctl hibernate'
-	elif [[ "$1" == '--opt5' ]]; then
-		confirm_run 'systemctl reboot'
-	elif [[ "$1" == '--opt6' ]]; then
-		confirm_run 'systemctl poweroff'
+	selected="$(confirm_exit)"
+	if [[ "$selected" == "$yes" ]]; then
+		if [[ $1 == '--shutdown' ]]; then
+			loginctl poweroff
+		elif [[ $1 == '--reboot' ]]; then
+			loginctl reboot
+		elif [[ $1 == '--suspend' ]]; then
+			mpc -q pause
+			amixer set Master mute
+			loginctl suspend
+		elif [[ $1 == '--logout' ]]; then
+			if [[ "$DESKTOP_SESSION" == 'openbox' ]]; then
+				openbox --exit
+			elif [[ "$DESKTOP_SESSION" == 'bspwm' ]]; then
+				bspc quit
+			elif [[ "$DESKTOP_SESSION" == 'i3' ]]; then
+				i3-msg exit
+			elif [[ "$DESKTOP_SESSION" == 'plasma' ]]; then
+				qdbus org.kde.ksmserver /KSMServer logout 0 0 0
+			elif [[ "$DESKTOP_SESSION" == 'dwm' ]]; then
+				killall dwm
+			fi
+		fi
+	else
+		exit 0
 	fi
 }
 
 # Actions
 chosen="$(run_rofi)"
 case ${chosen} in
-    $option_1)
-		run_cmd --opt1
+    $shutdown)
+		run_cmd --shutdown
         ;;
-    $option_2)
-		run_cmd --opt2
+    $reboot)
+		run_cmd --reboot
         ;;
-    $option_3)
-		run_cmd --opt3
+    $lock)
+		if [[ -x '/usr/bin/betterlockscreen' ]]; then
+			betterlockscreen -l
+		elif [[ -x '/usr/bin/i3lock' ]]; then
+			i3lock
+		elif [[ -x '/usr/bin/xscreensaver-command' ]]; then
+			if ! pgrep -x xscreensaver >/dev/null; then
+        		xscreensaver -no-splash &
+        		sleep 1
+    		fi
+			xscreensaver-command -lock
+		elif [[ -x '/usr/bin/slock' ]]; then
+			slock
+		fi
         ;;
-    $option_4)
-		run_cmd --opt4
+    $suspend)
+		run_cmd --suspend
         ;;
-    $option_5)
-		run_cmd --opt5
-        ;;
-    $option_6)
-		run_cmd --opt6
+    $logout)
+		run_cmd --logout
         ;;
 esac
-

f2c359939fff0c4ffbf40d694df07ae8900a01a2
Nathaniel Russell <46272571+nater1983@users.noreply.github.com>
2026-08-03 21:04:22 -0500

    Refactor Wi-Fi launcher script for improved clarity

--- files/scripts/wifi_launcher.sh
+++ files/scripts/wifi_launcher.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
+
 notify-send "Getting list of available Wi-Fi networks..."
+
 nmcli device wifi rescan 2>/dev/null
 sleep 2
 
@@ -10,32 +12,45 @@ elif [[ "$connected" =~ "disabled" ]]; then
 	toggle="直  Enable Wi-Fi"
 fi
 
-# Terse, colon-separated output -- stable regardless of field width/content.
-# Escaped colons in SSIDs (nmcli escapes ':' as '\:') are left alone here.
-wifi_list=$(nmcli -t --fields SECURITY,SSID device wifi list | \
+# Build colored list using terse output (stable field parsing) + signal-based color/bars
+wifi_list=$(nmcli -t --fields SECURITY,SIGNAL,SSID device wifi list | \
 	awk -F: '
 		{
-			ssid = $2
-			for (i = 3; i <= NF; i++) ssid = ssid ":" $i  # rejoin if SSID had a colon
+			ssid = $3
+			for (i = 4; i <= NF; i++) ssid = ssid ":" $i   # rejoin SSID if it contained a colon
 			if (ssid == "") next
+
 			sec = $1
 			if (sec == "" || sec == "--") {
 				icon = ""      # open network icon
 			} else {
 				icon = ""      # locked network icon
-			}
-			print icon "  " ssid
+		
+			signal = $2 + 0
+
+			icon = (sec == "" || sec == "--") ? "直" : "睊"
+
+			# Bars: 4-level block indicator based on signal strength
+			if (signal >= 80) { bars = "▂▄▆█"; color = "#8BC34A" }       # excellent - green
+			else if (signal >= 60) { bars = "▂▄▆_"; color = "#FFEB3B" } # good - yellow
+			else if (signal >= 40) { bars = "▂▄__"; color = "#FF9800" } # fair - orange
+			else { bars = "▂___"; color = "#E91E63" }                   # weak - pink/red
+
+			printf "<span foreground=\x27%s\x27>%s  %-4s %3d%%  %s</span>\n", color, icon, bars, signal, ssid
 		}
 	' | awk '!seen[$0]++')
 
-chosen_network=$(echo -e "$toggle\n$wifi_list" | rofi -dmenu -i -selected-row 1 -p "Wi-Fi SSID: ")
-chosen_id=$(echo "${chosen_network:3}" | xargs)
+# Use rofi to select wifi network (markup-rows enables the Pango color spans)
+chosen_network=$(echo -e "$toggle\n$wifi_list" | rofi -dmenu -i -markup-rows -selected-row 1 -p "Wi-Fi SSID: ")
+
+# Strip Pango markup, then strip icon/bars/percent columns to get the plain SSID
+chosen_id=$(echo "$chosen_network" | sed -E 's/<[^>]*>//g' | sed -E 's/^\S+\s+\S+\s+[0-9]+%\s+//' | xargs)
 
 if [ "$chosen_network" = "" ]; then
 	exit
-elif [ "$chosen_network" = "直  Enable Wi-Fi" ]; then
+elif [[ "$chosen_network" == *"Enable Wi-Fi"* ]]; then
 	nmcli radio wifi on
-elif [ "$chosen_network" = "睊  Disable Wi-Fi" ]; then
+elif [[ "$chosen_network" == *"Disable Wi-Fi"* ]]; then
 	nmcli radio wifi off
 else
 	success_message="You are now connected to the Wi-Fi network \"$chosen_id\"."
